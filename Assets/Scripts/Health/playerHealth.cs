90a68c7461a0908fd107ca836ebc8a4a295cc432
SechabaVDM <162897708+SechabaVDM@users.noreply.github.com>
2025-10-29 22:53:57 +0200

    Setting UI, HUD, Bubbles,Trash

---
+++ Assets/Scripts/Health/playerHealth.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxHealth = health; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
